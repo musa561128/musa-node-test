582ca40397beef7b438538eefe9f8217d692a3d5
CoderDojoObu <osamu@kawadaosamunoMacBook-ea.local>
2020-04-19 05:29:07 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var path = require('path');
+var morgan = require('morgan');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(morgan("combined"));
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    return res.render("index");
+});
+
+app.post("/" ,function(req, res){
+    return res.render("result",{username: req.body.username, message: req.body.message})
+});
+
+var server = http.createServer(app);
+server.listen(3000);
